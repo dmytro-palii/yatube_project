bdd62ac79efc813915dc8d4f0bdde36e5df427c8
dmytro-palii <74836389+dmytro-palii@users.noreply.github.com>
2022-02-23 05:31:11 +0000

    Connect views and urls

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Main Page")
+
+def group_posts(request, slug):
+    return HttpResponse(f"GROUPS: {slug}")
