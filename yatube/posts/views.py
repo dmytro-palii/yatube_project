670c5b1fb54c32de76764e5044f9371386ae6792
dmytro-palii <74836389+dmytro-palii@users.noreply.github.com>
2022-02-27 17:04:47 +0000

    Add html-templates tags

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,23 @@
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     template = "posts/index.html"
-    return render(request, template)
+    title = "POSTS"
+    context = {
+        "title" : title,
+        "text"  : "Это главная страница проекта Yatube"
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f"GROUPS: {slug}")
+    template = "posts/index.html"
+    title = "POSTS"
+    context = {
+        "title" : title,
+        "text"  : "Здесь будет информация о группах проекта Yatube"
+    }
+    return render(request, template, context)
+    # return HttpResponse(f"GROUPS: {slug}")
